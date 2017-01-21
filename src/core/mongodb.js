b42f4338540ef2e4a824805896f5879a06de3630
Anne Remacle <anne.m.remacle@gmail.com>
2017-01-21 14:07:03 +0100

    Connecting to mongoDB

---
+++ src/core/mongodb.js
@@ -0,0 +1,32 @@
+/* RIE/egzamen
+ *
+ * /src/core/mongodb.js - Connector for mongodb
+ *
+ * coded by Anne
+ * started at 21/01/2017
+ */
+
+ import { MongoClient } from "mongodb";
+ import Promise from "bluebird";
+
+ const MONGO_URL = "mongodb://127.0.0.1:27017/egzamen";
+
+ let oDB,
+    fInit;
+
+fInit = function() {
+    return new Promise( ( fResolve, fReject ) => {
+        MongoClient.connect( MONGO_URL, ( oError, oLinkedDB ) => {
+            if ( oError ) {
+                return fReject( oError );
+            }
+
+            fResolve( oDB = oLinkedDB );
+        } );
+    } );
+};
+
+export {
+    fInit as init,
+    oDB as db,
+};
