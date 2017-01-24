064e5549985bc7a18c7ff311542faef861ad0574
Anne Remacle <anne.m.remacle@gmail.com>
2017-01-24 11:40:06 +0100

    Implement restos list with distances

--- src/controllers/restos/list.js
+++ src/controllers/restos/list.js
@@ -8,14 +8,61 @@
 
 import getRestos from "../../models/restos";
 import { send, error } from "../../core/utils/api";
+import distance from "jeyo-distans";
+import checkPosition from "../../core/utils/position";
+
+const ARC_KILOMETER = 0.009259,
+    DEFAULT_RADIUS = 5,
+    MAX_RADIUS = 20;
 
 export default function( oRequest, oResponse ) {
 
- getRestos()
-    .find()
-    .toArray()
-    .then( ( aRestos ) => {
-         send( oRequest, oResponse, aRestos );
-    } )
-    .catch( ( oError ) => error( oRequest, oResponse, oError ) );
+    let oCurrentPosition = checkPosition( +oRequest.query.latitude, +oRequest.query.longitude ),
+        iSearchRadius = +oRequest.query.radius;
+
+        console.log(oCurrentPosition);
+
+    if ( !oCurrentPosition ) {
+        return error( oRequest, oResponse, "Invalid Position!", 400 );
+    }
+
+    if ( isNaN( iSearchRadius ) ) {
+        iSearchRadius = DEFAULT_RADIUS;
+    }
+    if ( iSearchRadius < DEFAULT_RADIUS ) {
+        iSearchRadius = DEFAULT_RADIUS;
+    }
+    if ( iSearchRadius > MAX_RADIUS ) {
+        iSearchRadius = MAX_RADIUS;
+    }
+
+    iSearchRadius *= ARC_KILOMETER;
+
+    getRestos()
+        .find( {
+            "latitude": {
+                "$gt": oCurrentPosition.latitude - iSearchRadius,
+                "$lt": oCurrentPosition.latitude + iSearchRadius,
+            },
+            "longitude": {
+                "$gt": oCurrentPosition.longitude - iSearchRadius,
+                "$lt": oCurrentPosition.longitude + iSearchRadius,
+            },
+            "deleted_at": null,
+        })
+        .toArray()
+        .then( ( aRestos = [] ) => {
+             let aCleanRestos;
+
+             aCleanRestos = aRestos.map( ( { _id, name, slug, latitude, longitude, address, hours } ) => ( {
+                 "id": _id,
+                 "distance": distance( oCurrentPosition, { latitude, longitude } ) * 1000,
+                 name, latitude, longitude, address, hours
+             } ) );
+
+            // On trie les restos selon la distance à laquelle ils se trouvent
+             aCleanRestos.sort( ( oRestoOne, oRestoTwo ) => oRestoOne.distance - oRestoTwo.distance );
+             send( oRequest, oResponse, aCleanRestos );
+        } )
+        .catch( ( oError ) => error( oRequest, oResponse, oError ) );
 }
