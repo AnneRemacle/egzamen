49b36bacccee0f845abd96ec0c6a0fee51e40304
Anne Remacle <anne.m.remacle@gmail.com>
2017-01-24 18:22:44 +0100

    Install dependencies & add base dummy modules

---
+++ static/modules/main.js
@@ -0,0 +1,13 @@
+/* RIA/egzamen
+ *
+ * /static/modules/main.js - Maint entry file
+ *
+ * coded by Anne
+ * started at 24/01/2017
+ */
+
+ import utils from "./utils";
+
+ console.log( "Hey you!" );
+
+ utils();
---
+++ static/modules/utils.js
@@ -0,0 +1,11 @@
+/* RIA/egzamen
+ *
+ * /static/modules/utils.js - Small utils
+ *
+ * coded by Anne
+ * started at 24/01/2017
+ */
+
+ export default function() {
+     console.log( "Hello from utils!" );
+ }
