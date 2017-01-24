431adbe3553440ba86bd18345b3f77531f97d4c4
Anne Remacle <anne.m.remacle@gmail.com>
2017-01-24 14:40:30 +0100

    removing useless spaces

--- src/routes/system.js
+++ src/routes/system.js
@@ -6,16 +6,16 @@
  * started at 21/10/2017
  */
 
- import { Router } from "express";
+import { Router } from "express";
 
 import sysPingController from "../controllers/system/ping.js";
 import sysEchoController from "../controllers/system/echo.js";
 import sysErrorController from "../controllers/system/error.js";
 
- let oRouter = new Router();
+let oRouter = new Router();
 
- oRouter.get( "/sys/ping", sysPingController );
+oRouter.get( "/sys/ping", sysPingController );
 oRouter.get( "/sys/echo", sysEchoController );
 oRouter.get( "sys/error", sysErrorController );
 
- export default oRouter;
+export default oRouter;
